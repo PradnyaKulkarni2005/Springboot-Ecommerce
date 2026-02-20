22e200f5fd7dc3dfc8aebd231d2ee8472e210da6
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-02-20 21:46:11 +0530

    Learned how springboot creates object using ApplicationContext

--- src/main/java/com/example/ecommerce/Dev.java
+++ src/main/java/com/example/ecommerce/Dev.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce;
-
+import org.springframework.stereotype.Component;
+@Component
 public class Dev {
     public void build(){
         System.out.println("Building the project in Dev mode...");
--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -2,13 +2,14 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
-		Dev dev = new Dev();
+		ApplicationContext context=SpringApplication.run(EcommerceApplication.class, args);
+		Dev dev = context.getBean(Dev.class);
 		dev.build();
 	}
 
