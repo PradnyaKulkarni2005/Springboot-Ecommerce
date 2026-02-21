b2aa26c2c1f8cfb9cb4c931279b5567968efa0b7
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-02-21 10:44:58 +0530

    use of autowired annotation

--- src/main/java/com/example/ecommerce/Dev.java
+++ src/main/java/com/example/ecommerce/Dev.java
@@ -1,9 +1,16 @@
 package com.example.ecommerce;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 @Component
 public class Dev {
+    @Autowired
+    private Laptop laptop;
+
     public void build(){
+        laptop.compile();
         System.out.println("Building the project in Dev mode...");
+        
     }
+
     
 }
---
+++ src/main/java/com/example/ecommerce/Laptop.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce;
+import org.springframework.stereotype.Component;
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Compiling the laptop code...");
+    }
+    
+}
